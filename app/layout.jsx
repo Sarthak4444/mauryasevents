0ce3444151e666be74da3dc7a662422ace73766b
Sarthak4444 <maursarthak100@gmail.com>
2025-02-01 07:55:56 +0530

    hhs

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,17 +4,17 @@ import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 
 export const metadata = {
-  title: "Maurya's Private Dining",
+  title: "Maurya's Events",
   description:
-    "Welcome to Maurya's Private Dining, where exquisite flavors meet exceptional ambiance. Explore our menu, reserve a table, and enjoy an unforgettable dining experience.",
+    "Welcome to Maurya's Events, where exquisite flavors meet exceptional ambiance. Explore our menu, reserve a table, and enjoy an unforgettable dining experience.",
   keywords:
     "restaurant, dining, fine dining, family dining, best restaurant, Maurya's, food and drinks, table reservations",
   openGraph: {
-    title: "Maurya's Private Dining",
+    title: "Maurya's Evenys",
     description:
-      "Experience the perfect blend of flavors and ambiance at Maurya's Private Dining. Browse our menu and reserve your table today!",
-    url: "https://www.mauryascuisine.com",
-    siteName: "Maurya's Private Dining",
+      "Experience the perfect blend of flavors and ambiance at Maurya's Events. Browse our menu and reserve your table today!",
+    url: "https://www.mauryasevents.com",
+    siteName: "Maurya's Events",
     locale: "en_US",
     type: "website",
   },
