a51e97ca0608b3c34b6788a3404aed9d1bf7aa79
Sarthak4444 <maursarthak100@gmail.com>
2025-02-01 09:33:07 +0530

    7

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,25 +1,19 @@
 "use client";
 import React from "react";
 import Link from "next/link";
-// import Logo from "./Logo.png"
+import Logo from "./Logo.png"
+import Image from "next/image";
 
 function Header() {
 
   return (
     <div className="sticky top-0 left-0 right-0 z-40">
-      <div className="w-full flex justify-between items-center md:px-14 px-5 py-5 bg-black">
+      <div className="w-full flex justify-start items-center md:px-14 px-5 py-5 bg-black">
         <div>
           <Link href="/">
-          {/* <img src= {Logo} alt="Logo" width="112" height="250" /> */}
+          <Image src= {Logo} className="cursor-pointer w-28 h-auto" alt="Logo" width="112" height="250" />
 
           </Link>
-        </div>
-        <div>
-          <button
-            className="bg-black p-2 md:p-4 px-4 md:px-10 font-semibold border-[1px] border-[#d88728] text-white hover:bg-[#d88728] transition-all duration-400"
-          >
-            RESERVE NOW
-          </button>
         </div>
       </div>
     </div>
