a3db9ddb187f2f4f675ffecc0c05d67b46778a94
Sarthak4444 <maursarthak100@gmail.com>
2025-02-01 10:04:29 +0530

    ug

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
 
   return (
     <div className="sticky top-0 left-0 right-0 z-40">
-      <div className="w-full flex justify-start items-center bg-black">
+      <div className="w-full flex justify-start items-center bg-black py-5 px-10">
           <Link href="/">
           <Image src= {Logo} width={200} height={100} alt="Logo" />
           </Link>
