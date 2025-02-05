2ce2d647dd4a90a9ce4c5f7dc2d5bafbb72a4324
Sarthak4444 <maursarthak100@gmail.com>
2025-02-05 13:28:57 +0530

    tf

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -9,12 +9,10 @@ function Footer() {
     <div className="bg-black text-white">
       <div className="flex flex-col p-10 justify-center items-center">
         <Image src={Pattern} alt="Pattern" width={200} height={200} />
-        <div className="text-left w-full mb-8 -mt-4">
-          {"              "}
-          <Link className="text-sm underline" href="/terms-and-conditions">
+        <div className="text-left ml-10 pl-10 w-full mb-8 -mt-4">
+          <Link className="ml-10 pl-10 text-sm underline" href="/terms-and-conditions">
             Terms & Conditions
           </Link>
-          {"              "}
           <Link className="text-sm underline" href="/privacy-policy">
             Privacy Policy
           </Link>
