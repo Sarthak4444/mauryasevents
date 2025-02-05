333ea4332be8adfd073ce194de26cb84ba1fdd43
Sarthak4444 <maursarthak100@gmail.com>
2025-02-05 14:33:40 +0530

    hu

--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,6 @@ const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
 );
 
-
 export default function Home() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
