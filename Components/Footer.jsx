0b9cadd2542c3dfe70e0756ed2e76995930d4623
Sarthak4444 <maursarthak100@gmail.com>
2025-02-05 13:26:15 +0530

    j

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,28 +1,23 @@
 "use client";
 import React from "react";
 import Link from "next/link";
-import Pattern from "./Pattern.png"
+import Pattern from "./Pattern.png";
 import Image from "next/image";
 
 function Footer() {
-
   return (
     <div className="bg-black text-white">
       <div className="flex flex-col p-10 justify-center items-center">
-        <Image
-          src={Pattern}
-          alt="Pattern"
-          width={200}
-          height={200}
-        />
+        <Image src={Pattern} alt="Pattern" width={200} height={200} />
         <div className="text-left w-full mb-8 -mt-4">
+          {"              "}
           <Link className="text-sm underline" href="/terms-and-conditions">
-          {"     "}  Terms & Conditions {"     "}
+            Terms & Conditions
           </Link>
+          {"              "}
           <Link className="text-sm underline" href="/privacy-policy">
             Privacy Policy
           </Link>
-        
         </div>
       </div>
     </div>
