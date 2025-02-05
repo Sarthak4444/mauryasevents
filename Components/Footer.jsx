b200203433a47ddce7d65e38fc038f6e2091efe0
Sarthak4444 <maursarthak100@gmail.com>
2025-02-05 13:10:59 +0530

    uh

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 function Footer() {
 
   return (
-    <div className="py-10 bg-black text-white">
+    <div className="py-10 bg-black text-white p-8">
       <div className="flex flex-col gap- p-10 justify-center items-center">
         <Image
           src={Pattern}
@@ -15,8 +15,8 @@ function Footer() {
           width={200}
           height={200}
         />
-        <div className="text-left w-full flex gap-5 pl-10 mb-10">
-          <Link className="text-sm underline" href="/terms-and-conditions">
+        <div className="text-left w-full flex">
+          <Link className="text-sm underline pr-10" href="/terms-and-conditions">
             Terms & Conditions 
           </Link>
           <Link className="text-sm underline" href="/privacy-policy">
