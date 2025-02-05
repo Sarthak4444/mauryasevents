82cd4d5afba965cc463f19b4199a2023db04a65b
Sarthak4444 <maursarthak100@gmail.com>
2025-02-05 12:56:07 +0530

    jh

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -8,17 +8,17 @@ function Footer() {
 
   return (
     <div className="py-10 bg-black text-white">
-      <div className="flex flex-col gap-6 md:gap-10 p-10 justify-center items-center">
+      <div className="flex flex-col gap- p-10 justify-center items-center">
         <Image
           src={Pattern}
           alt="Pattern"
           width={200}
           height={200}
         />
-        <div className="text-left w-full">
+        <div className="text-left w-full m-5">
           <Link className="text-sm underline mr-8" href="/terms-and-conditions">
             Terms & Conditions 
-          </Link>
+          </Link>3
           <Link className="text-sm underline" href="/privacy-policy">
             Privacy Policy
           </Link>
