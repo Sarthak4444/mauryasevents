f012634a3893d5ba12dd7e5c15dcb1d3263cd22d
Sarthak4444 <maursarthak100@gmail.com>
2025-02-07 14:57:24 +0530

    hu

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,19 +1,29 @@
 
 import "./globals.css";
+import Item from "./Item.png";
+
 export const metadata = {
   title: "Maurya's Events",
   description:
-    "Welcome to Maurya's Events, where exquisite flavors meet exceptional ambiance. Explore our menu, reserve a table, and enjoy an unforgettable dining experience.",
+    "Welcome to Maurya's Events, explore our menu, reserve a table, and enjoy an unforgettable dining experience.",
   keywords:
-    "restaurant, dining, fine dining, family dining, best restaurant, Maurya's, food and drinks, table reservations",
+    "restaurant, dining, fine dining, family dining, tapas, cocktails, champagne, beer, beers, brewery, best cocktail bar, kamloops, mixology, best restaurant, Maurya's, food and drinks, table reservations",
   openGraph: {
-    title: "Maurya's Evenys",
+    title: "Maurya's Events",
     description:
-      "Experience the perfect blend of flavors and ambiance at Maurya's Events. Browse our menu and reserve your table today!",
+      "One dish. One drink. One unforgettable moment at a time.",
     url: "https://www.mauryasevents.com",
     siteName: "Maurya's Events",
     locale: "en_US",
     type: "website",
+    images: [
+      {
+        url: Item,
+        width: 800,
+        height: 600,
+        alt: "Maurya's Events",
+      },
+    ],  
   },
 };
 
