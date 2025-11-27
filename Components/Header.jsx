3c9fb06a61f1de750498e10611d83ae76d5dbc92
Sarthak4444 <168180160+Sarthak4444@users.noreply.github.com>
2025-11-26 20:24:33 -0800

    Refactor Header component to remove duplicates
    Removed duplicate div elements displaying '000000' in the header.

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -9,8 +9,8 @@ function Header() {
   return (
     <div className="sticky top-0 left-0 right-0 z-40">
       <div className="w-full flex justify-start items-center h-5 mb-5 bg-black py-10 px-10">
-        <div className="text-3xl my-4 text-black">000000</div>
-        <div className="text-3xl my-4 text-black">000000</div>
+        <div className="text-3xl my-4 text-black">000000</div> <br>
+        <div className="text-3xl my-4 text-black">000000</div> <br>
         <div className="text-3xl my-4 text-black">000000</div>
            {/*<Link href="/">
           <Image src= {Logo} width={150} height={100} alt="Logo" />
