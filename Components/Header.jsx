c316d8c2ff2b50108fc8317673a932ae30be5bc5
Sarthak4444 <168180160+Sarthak4444@users.noreply.github.com>
2025-11-26 20:21:06 -0800

    Change span to div for header textubb

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
   return (
     <div className="sticky top-0 left-0 right-0 z-40">
       <div className="w-full flex justify-start items-center h-5 mb-5 bg-black py-5 px-10">
-        <span className="text-3xl my-4 text-black">000000<br><br><br><br>00000</span>
+        <div className="text-3xl my-4 text-black">000000<br><br><br><br>00000</div>
            {/*<Link href="/">
           <Image src= {Logo} width={150} height={100} alt="Logo" />
           </Link>
